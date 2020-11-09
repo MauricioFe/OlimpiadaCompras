a52563858fa5115ea0540208510ab89a993203a0
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-11-09 10:33:25 -0300

    Finalizando primeira etapa

--- OlimpiadaCompras/Properties/Resources.Designer.cs
+++ OlimpiadaCompras/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace OlimpiadaCompras.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Capturar {
+            get {
+                object obj = ResourceManager.GetObject("Capturar", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap olimpiada_do_conhecimento {
             get {
                 object obj = ResourceManager.GetObject("olimpiada-do-conhecimento", resourceCulture);
--- OlimpiadaCompras/Telas/Coordenacao/FrmEmailAutorizacao.Designer.cs
+++ OlimpiadaCompras/Telas/Coordenacao/FrmEmailAutorizacao.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmEmailAutorizacao));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnAprovar = new System.Windows.Forms.Button();
             this.btnReprvar = new System.Windows.Forms.Button();
@@ -37,7 +36,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::OlimpiadaCompras.Properties.Resources.Capturar;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(775, 584);
