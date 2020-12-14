dfa8a436a9bec69ac20553b722771950433bb16e
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-12-14 11:07:56 -0300

    Finalizando o crud de ocupações

--- OlimpiadaCompras/Telas/Coordenacao/Cadastros/FrmCadastroOcupacoes.Designer.cs
+++ OlimpiadaCompras/Telas/Coordenacao/Cadastros/FrmCadastroOcupacoes.Designer.cs
@@ -29,17 +29,22 @@
         private void InitializeComponent()
         {
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
             this.btnEditar = new System.Windows.Forms.Button();
-            this.btnCancelar = new System.Windows.Forms.Button();
-            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtOcupacao = new System.Windows.Forms.TextBox();
+            this.dgvOcupacoes = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox36 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.txtAnexos = new System.Windows.Forms.TextBox();
+            this.btnSelecionar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFiltro = new System.Windows.Forms.TextBox();
+            this.colNumero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNomeOcupacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colIdOcupacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOcupacoes)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,13 +57,13 @@
             this.label2.TabIndex = 68;
             this.label2.Text = "Número";
             // 
-            // textBox2
+            // txtNumero
             // 
-            this.textBox2.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(359, 52);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(378, 29);
-            this.textBox2.TabIndex = 67;
+            this.txtNumero.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(359, 52);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(378, 29);
+            this.txtNumero.TabIndex = 67;
             // 
             // btnEditar
             // 
@@ -72,32 +77,35 @@
             this.btnEditar.TabIndex = 66;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
-            // btnCancelar
+            // btnExcluir
             // 
-            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(17)))), ((int)(((byte)(54)))));
-            this.btnCancelar.FlatAppearance.BorderSize = 0;
-            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCancelar.ForeColor = System.Drawing.Color.White;
-            this.btnCancelar.Location = new System.Drawing.Point(595, 384);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(142, 40);
-            this.btnCancelar.TabIndex = 65;
-            this.btnCancelar.Text = "Excluir";
-            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnExcluir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(17)))), ((int)(((byte)(54)))));
+            this.btnExcluir.FlatAppearance.BorderSize = 0;
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.ForeColor = System.Drawing.Color.White;
+            this.btnExcluir.Location = new System.Drawing.Point(595, 384);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(142, 40);
+            this.btnExcluir.TabIndex = 65;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
             // 
-            // btnEntrar
+            // btnSalvar
             // 
-            this.btnEntrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(166)))), ((int)(((byte)(90)))));
-            this.btnEntrar.FlatAppearance.BorderSize = 0;
-            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEntrar.ForeColor = System.Drawing.Color.White;
-            this.btnEntrar.Location = new System.Drawing.Point(595, 157);
-            this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(142, 40);
-            this.btnEntrar.TabIndex = 64;
-            this.btnEntrar.Text = "Salvar";
-            this.btnEntrar.UseVisualStyleBackColor = false;
+            this.btnSalvar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(166)))), ((int)(((byte)(90)))));
+            this.btnSalvar.FlatAppearance.BorderSize = 0;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(595, 157);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(142, 40);
+            this.btnSalvar.TabIndex = 64;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // label1
             // 
@@ -108,26 +116,31 @@
             this.label1.TabIndex = 63;
             this.label1.Text = "Nome da ocupação";
             // 
-            // textBox1
+            // txtOcupacao
             // 
-            this.textBox1.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(68, 52);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(285, 29);
-            this.textBox1.TabIndex = 62;
+            this.txtOcupacao.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOcupacao.Location = new System.Drawing.Point(68, 52);
+            this.txtOcupacao.Name = "txtOcupacao";
+            this.txtOcupacao.Size = new System.Drawing.Size(285, 29);
+            this.txtOcupacao.TabIndex = 62;
             // 
-            // dataGridView1
+            // dgvOcupacoes
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(68, 203);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(669, 175);
-            this.dataGridView1.TabIndex = 61;
+            this.dgvOcupacoes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOcupacoes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colNumero,
+            this.colNomeOcupacao,
+            this.colIdOcupacao});
+            this.dgvOcupacoes.Location = new System.Drawing.Point(68, 203);
+            this.dgvOcupacoes.Name = "dgvOcupacoes";
+            this.dgvOcupacoes.Size = new System.Drawing.Size(669, 175);
+            this.dgvOcupacoes.TabIndex = 61;
+            this.dgvOcupacoes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvOcupacoes_CellClick);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox36);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtAnexos);
+            this.groupBox1.Controls.Add(this.btnSelecionar);
             this.groupBox1.Location = new System.Drawing.Point(68, 87);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(669, 64);
@@ -135,43 +148,83 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Anexos";
             // 
-            // textBox36
+            // txtAnexos
             // 
-            this.textBox36.Location = new System.Drawing.Point(6, 28);
-            this.textBox36.Name = "textBox36";
-            this.textBox36.Size = new System.Drawing.Size(515, 29);
-            this.textBox36.TabIndex = 32;
+            this.txtAnexos.Location = new System.Drawing.Point(6, 28);
+            this.txtAnexos.Name = "txtAnexos";
+            this.txtAnexos.Size = new System.Drawing.Size(515, 29);
+            this.txtAnexos.TabIndex = 32;
             // 
-            // button1
+            // btnSelecionar
             // 
-            this.button1.Location = new System.Drawing.Point(527, 28);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 29);
-            this.button1.TabIndex = 33;
-            this.button1.Text = "Selecionar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSelecionar.Location = new System.Drawing.Point(527, 28);
+            this.btnSelecionar.Name = "btnSelecionar";
+            this.btnSelecionar.Size = new System.Drawing.Size(129, 29);
+            this.btnSelecionar.TabIndex = 33;
+            this.btnSelecionar.Text = "Selecionar";
+            this.btnSelecionar.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 22);
+            this.label4.TabIndex = 71;
+            this.label4.Text = "Filtro";
+            // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFiltro.Location = new System.Drawing.Point(127, 168);
+            this.txtFiltro.Name = "txtFiltro";
+            this.txtFiltro.Size = new System.Drawing.Size(261, 29);
+            this.txtFiltro.TabIndex = 70;
+            this.txtFiltro.TextChanged += new System.EventHandler(this.txtFiltro_TextChanged);
+            // 
+            // colNumero
+            // 
+            this.colNumero.HeaderText = "Número";
+            this.colNumero.Name = "colNumero";
+            this.colNumero.ReadOnly = true;
+            this.colNumero.Width = 200;
+            // 
+            // colNomeOcupacao
+            // 
+            this.colNomeOcupacao.HeaderText = "Ocupação";
+            this.colNomeOcupacao.Name = "colNomeOcupacao";
+            this.colNomeOcupacao.ReadOnly = true;
+            this.colNomeOcupacao.Width = 424;
+            // 
+            // colIdOcupacao
+            // 
+            this.colIdOcupacao.HeaderText = "id";
+            this.colIdOcupacao.Name = "colIdOcupacao";
+            this.colIdOcupacao.ReadOnly = true;
+            this.colIdOcupacao.Visible = false;
             // 
             // FrmCadastroOcupacoes
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFiltro);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtNumero);
             this.Controls.Add(this.btnEditar);
-            this.Controls.Add(this.btnCancelar);
-            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtOcupacao);
+            this.Controls.Add(this.dgvOcupacoes);
             this.Font = new System.Drawing.Font("Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmCadastroOcupacoes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FrmCadastroOcupacoes";
-            this.Load += new System.EventHandler(this.FrmCadastroOcupacoes_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "Cadastrar Ocupações";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOcupacoes)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -182,15 +235,20 @@
         #endregion
 
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtNumero;
         private System.Windows.Forms.Button btnEditar;
-        private System.Windows.Forms.Button btnCancelar;
-        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnSalvar;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtOcupacao;
+        private System.Windows.Forms.DataGridView dgvOcupacoes;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox36;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAnexos;
+        private System.Windows.Forms.Button btnSelecionar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFiltro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNumero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNomeOcupacao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colIdOcupacao;
     }
 }
