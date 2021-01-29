c8fb3eb6f8982b724f3310fd8eaed6446630e1b8
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-01-28 22:39:42 -0300

    Criando método para consumir end point de envio de email

---
+++ OlimpiadaCompras/Requests/HttpEmail.cs
@@ -0,0 +1,31 @@
+﻿using OlimpiadaCompras.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace OlimpiadaCompras.Requests
+{
+    public static class HttpEmail
+    {
+        public static async Task EnviarEmail(EmailModel data, long idSolicitacao, string token)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+                    var json = new JavaScriptSerializer().Serialize(data);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
